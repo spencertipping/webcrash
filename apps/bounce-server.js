18fa715a5475c1126195b0ce130f0aae98120b79
Spencer Tipping <spencer@spencertipping.com>
2010-09-17 19:13:34 -0600

    Wrote a couple of clients for the echo server

--- apps/bounce-server.js
+++ apps/bounce-server.js
@@ -1,6 +1,14 @@
 var listeners = [];
 require('http').createServer (function (req, res) {
   /listen/.test(req.url) && listeners.push(res);
+
+  /app/.test(req.url) && (function () {
+    require('fs').readFile (req.url.split('/', 3)[2] + '.html', 'ascii', function (err, data) {
+      res.writeHead (200, {'Content-type': 'text/html'});
+      res.end (data);
+    });
+  }) ();
+
   /send/.test(req.url) && (function () {
     for (var i = 0, l = listeners.length; i < l; ++i) {
       listeners[i].writeHead (200, {'Content-type': 'text/plain'});
@@ -12,4 +20,4 @@ require('http').createServer (function (req, res) {
     res.writeHead (200, {'Content-type': 'text/plain'});
     res.end ('Sent message to listeners');
   }) ();
-}).listen (8080);
+}).listen (8080, '0.0.0.0');
